dc1e37626ad0ad722fb8f739da1047d77ce71416
NotMyPersonalAccount <kevinzj710@gmail.com>
2023-02-21 13:54:04 -0800

    big boi code

--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,147 @@
 use ev3dev_lang_rust::sensors::{GyroSensor, UltrasonicSensor};
 use ev3dev_lang_rust::{motors::LargeMotor, Ev3Result};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use uom::si::f32::Length;
 use uom::si::length::{centimeter, meter};
 
-const WHEEL_SPEED: i32 = 40;
+const DEFAULT_SPEED: i32 = 40;
 
-pub enum Direction {
-    LEFT,
-    RIGHT,
+pub enum Direction{
+	LEFT,
+	RIGHT,
 }
 
-pub fn move_distance(
-	wheels: (&LargeMotor, &LargeMotor),
-	radius: Length,
-	distance: Length,
-) -> Ev3Result<()> {
-	let (motor_left, motor_right) = wheels;
-	motor_left.set_duty_cycle_sp(WHEEL_SPEED)?;
-	motor_right.set_duty_cycle_sp(WHEEL_SPEED)?;
-
-	let rotations =
-		distance.get::<meter>() / (2.0 * std::f32::consts::PI * radius.get::<meter>());
-
-	let tachys = rotations * motor_left.get_count_per_rot()? as f32;
-
-	motor_left.set_position(0)?;
-	motor_right.set_position(0)?;
-
-    motor_right.run_direct()?;
-    motor_left.run_direct()?;
-
-	loop {
-		if motor_left.get_position()? >= tachys as i32 || motor_right.get_position()? >= tachys as i32 {
-			motor_left.stop()?;
-			motor_right.stop()?;
-			break;
+impl Direction{
+	fn sign(&self) -> i32 {
+		match self {
+			Direction::LEFT => -1,
+			Direction::RIGHT => 1,
 		}
 	}
-
-	Ok(())
 }
 
-pub fn turn(wheels: (&LargeMotor, &LargeMotor), gyro: &GyroSensor, angle: u16, direction: Direction) -> Ev3Result<()> {
-	let (motor_left, motor_right) = wheels;
-	
-	let sign = match direction {
-        Direction::LEFT => -1,
-        Direction::RIGHT => 1,
-    };
-
-    motor_left.set_duty_cycle_sp(WHEEL_SPEED * sign)?;
-    motor_right.set_duty_cycle_sp(-WHEEL_SPEED * sign)?;
-
-	gyro.set_mode_gyro_ang()?;
-	let initial_angle = gyro.get_angle()?;
-	let overshoot = 6; // tends to go further than the angle so this is a fun hackfix
-
-	motor_left.run_direct()?;
-	motor_right.run_direct()?;
-
-	while (gyro.get_angle()? - initial_angle).abs() < angle as i32 - overshoot {}
-	motor_left.stop()?;
-	motor_right.stop()?;
-
-	Ok(())
+pub struct Wheels{
+	pub motors: (LargeMotor, LargeMotor),
+	pub radius: Length,
 }
 
-pub fn turn_until<C>(wheels: (&LargeMotor, &LargeMotor),  direction: Direction, condition: C) -> Ev3Result<()>
-    where C: Fn() -> bool {
-
-	let (motor_left, motor_right) = wheels;
-
-	let sign = match direction {
-        Direction::LEFT => -1,
-        Direction::RIGHT => 1,
-    };
-
-    motor_left.set_duty_cycle_sp(WHEEL_SPEED * sign)?;
-    motor_right.set_duty_cycle_sp(-WHEEL_SPEED * sign)?;
-
-	motor_left.run_direct()?;
-	motor_right.run_direct()?;
-
-	while condition() {}
-	motor_left.stop()?;
-	motor_right.stop()?;
-
-	Ok(())
-
+pub struct Bot{
+	pub wheels: Wheels,
+	pub gyro: GyroSensor,
+	pub ultrasonic: UltrasonicSensor,
+	pub running: Arc<AtomicBool>,
 }
 
-pub fn get_obstacle_distance(
-	sensor: &UltrasonicSensor
-) -> Ev3Result<Length> {
-	Ok(Length::new::<centimeter>(sensor.get_distance_centimeters()? as f32))
+pub struct MoveOptions{
+	pub forward: bool,
+	pub speed: i32,
+}
+
+impl Default for MoveOptions{
+	fn default() -> Self {
+		Self {
+			forward: true,
+			speed: DEFAULT_SPEED,
+		}
+	}
+}
+
+pub struct TurnOptions{
+	pub speed: i32,
+	pub direction: Direction,
+}
+
+impl Default for TurnOptions{
+	fn default() -> Self {
+		Self {
+			speed: DEFAULT_SPEED,
+			direction: Direction::LEFT,
+		}
+	}
+}
+
+impl Bot{
+	//Moves the robot until the given condition is met.
+	pub fn move_until<C>(&self, options: &MoveOptions, until: C) -> Ev3Result<()>
+	where C: Fn() -> Ev3Result<bool> {
+		let (left, right) = &self.wheels.motors;
+		let speed = if options.forward { options.speed } else { -options.speed };
+		left.set_duty_cycle_sp(speed)?;
+		right.set_duty_cycle_sp(speed)?;
+
+		left.set_position(0)?;
+		right.set_position(0)?;
+
+		left.run_direct()?;
+		right.run_direct()?;
+
+		loop {
+			if until()? || !self.running.load(Ordering::SeqCst) {
+				self.stop_movement()?;
+				break;
+			}
+		}
+		
+		Ok(())
+	}
+
+	//Moves the robot by the given distance.
+	pub fn move_distance(&self, options: &MoveOptions, distance: Length) -> Ev3Result<()> {
+		let (left, right) = &self.wheels.motors;
+		self.move_until(
+			options,
+			|| {
+				let rotations =
+					distance.get::<meter>() / (2.0 * std::f32::consts::PI * self.wheels.radius.get::<meter>());
+				let tachys = rotations * left.get_count_per_rot()? as f32;
+				Ok(left.get_position()? >= tachys as i32 || right.get_position()? >= tachys as i32)
+			},
+		)
+	}
+
+	//Turns the robot until the given condition is met.
+	pub fn turn_until<C>(&self, options: &TurnOptions, until: C) -> Ev3Result<()> 
+	where C: Fn() -> Ev3Result<bool> {
+		let (left, right) = &self.wheels.motors;
+		left.set_duty_cycle_sp(options.speed * options.direction.sign())?;
+		right.set_duty_cycle_sp(options.speed * -options.direction.sign())?;
+
+		left.run_direct()?;
+		right.run_direct()?;
+
+		loop{
+			if until()? || !self.running.load(Ordering::SeqCst) {
+				self.stop_movement()?;
+				break;
+			}
+		}
+
+		Ok(())
+	}
+
+	//Turns the robot by the given angle in degrees.
+	pub fn turn_angle(&self, options: &TurnOptions, angle: u16) -> Ev3Result<()> {
+		self.gyro.set_mode_gyro_ang()?;
+		let initial_angle = self.gyro.get_angle()?;
+		self.turn_until(
+			options,
+			|| {
+				Ok(self.gyro.get_angle()? - initial_angle >= angle as i32)
+			},
+		)
+	}
+
+	//Stops the robot's movements.
+	pub fn stop_movement(&self) -> Ev3Result<()> {
+		let (left, right) = &self.wheels.motors;
+		left.stop()?;
+		right.stop()?;
+		Ok(())
+	}
+
+	//Get the distance from the obstacle in front of the robot.
+	pub fn get_distance_from_obstacle(&self) -> Ev3Result<Length> {
+		Ok(Length::new::<centimeter>(self.ultrasonic.get_distance_centimeters()?))
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,58 @@
 extern crate ev3dev_lang_rust;
 
+use ctrlc;
 use ev3dev_lang_rust::motors::{LargeMotor, MotorPort};
 use ev3dev_lang_rust::sensors::{GyroSensor, UltrasonicSensor};
+use ev3dev_lang_rust::sound;
 use ev3dev_lang_rust::Ev3Result;
 use uom::si::f32::Length;
 use uom::si::length::{centimeter, meter};
-use robocode::{Direction, get_obstacle_distance, move_distance, turn, turn_until};
+use robocode::{Bot, Direction, MoveOptions, TurnOptions, Wheels};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 fn main() -> Ev3Result<()> {
-    let wheels = (&LargeMotor::get(MotorPort::OutB)?, &LargeMotor::get(MotorPort::OutC)?);
-    wheels.0.stop()?;
-    wheels.1.stop()?;
-    let gyro_sensor = &GyroSensor::find()?;
-    let ultrasonic_sensor = &UltrasonicSensor::find()?;
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    });
 
-    loop {
-        //Keep turning 5 degrees left while there is something <5 cm away. 
-        //while get_obstacle_distance(&ultrasonic_sensor)? < Length::new::<centimeter>(5.0) {
-        //    turn(wheels, gyro_sensor, 5, Direction::LEFT);
-        //}
-        turn_until(wheels, Direction::LEFT, || {get_obstacle_distance(&ultrasonic_sensor).unwrap() < Length::new::<centimeter>(5.0)})?;
+    let bot = Bot {
+        wheels: Wheels {
+            motors: (LargeMotor::get(MotorPort::OutB)?, LargeMotor::get(MotorPort::OutC)?),
+            radius: Length::new::<meter>(0.02667)
+        },
+        gyro: GyroSensor::find()?,
+        ultrasonic: UltrasonicSensor::find()?,
+        running: running.clone(),
+    };
 
-        //Continuously move forward by 1cm while there is nothing in the way.
-        move_distance(wheels, Length::new::<meter>(0.02667), Length::new::<centimeter>(1.0))?;
+    sound::set_volume(100)?;
+    sound::play("/home/robot/Music/hey_marc.wav")?;
+
+    let move_options = MoveOptions {
+        forward: true,
+        speed: 60,
+    };
+    let back_options = MoveOptions {
+        forward: false,
+        speed: 40,
+    };
+    let turn_options = TurnOptions {
+        speed: 60,
+        direction: Direction::LEFT,
+    };
+    while running.load(Ordering::SeqCst) {
+        bot.turn_until(&turn_options, || {
+            Ok(bot.get_distance_from_obstacle()? > Length::new::<centimeter>(10.0))
+        })?;
+        bot.move_distance(&back_options, Length::new::<centimeter>(20.0))?;
+        bot.move_until(&move_options, || {
+            Ok(bot.get_distance_from_obstacle()? <= Length::new::<centimeter>(10.0))
+        })?;
     }
 
-	Ok(())
+    bot.stop_movement()?;
+    Ok(())
 }
